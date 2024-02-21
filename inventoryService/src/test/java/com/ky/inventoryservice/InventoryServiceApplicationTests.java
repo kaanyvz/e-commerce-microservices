55bd7e6dd6abf4d31bad216cdd7e1f4498160b9b
kaanyvz <kaan_yavuz12@hotmail.com>
2024-02-21 23:29:26 +0300

    Initial commit

---
+++ inventoryService/src/test/java/com/ky/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ky.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rabbitService/src/test/java/com/ky/rabbitservice/RabbitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ky.rabbitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
